ed837288c740efc7292765daf3b372a630f6d7ec
SuperDev348 <vladimir.danil25@mail.ru>
2023-03-30 07:40:04 +0200

    update

--- src/api/nft.js
+++ src/api/nft.js
@@ -11,18 +11,6 @@ async function owner() {
   }
 }
 
-async function mint(amount, address, value) {
-  try {
-    const contractInstance = getContractInstance()
-    const res = await contractInstance.methods.mint(amount).send({
-      from: address,
-      value: value
-    });
-    return Promise.resolve(res);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
 
 export {
   mint,
