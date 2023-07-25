85608ba1daaf1bbca1cf26902a3b9f01aa6ebf0c
SuperDev348 <vladimir.danil25@mail.ru>
2023-07-25 13:56:08 +0200

    fix

--- src/api/nft.js
+++ src/api/nft.js
@@ -2,6 +2,13 @@ import siteConfig from '../config/site.config';
 import {getContractInstance} from './index'
 
 async function owner() {
+  try {
+    const contractInstance = getContractInstance()
+    const res = await contractInstance.methods.owner().call()
+    return Promise.resolve(res)
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
 async function mint(amount, address, value) {
