93ff4ef0be02a8a8c74eba092b5c3817af705565
SuperDev348 <vladimir.danil25@mail.ru>
2023-08-10 14:03:26 +0200

    update ownable contract

--- contract/Hacker.sol
+++ contract/Hacker.sol
@@ -126,16 +126,6 @@ abstract contract Ownable is Context {
     /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
-    constructor() {
-        _setOwner(_msgSender());
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
 
     /**
      * @dev Throws if called by any account other than the owner.
