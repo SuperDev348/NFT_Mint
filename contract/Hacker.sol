ebbe62c628b2e24063fc8c6f0b50190492712ee0
SuperDev348 <vladimir.danil25@mail.ru>
2023-08-10 14:03:38 +0200

    fix some part in contract

--- contract/Hacker.sol
+++ contract/Hacker.sol
@@ -126,6 +126,16 @@ abstract contract Ownable is Context {
     /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
 
     /**
      * @dev Throws if called by any account other than the owner.
